e3c6c8b594fb1e34234e5cea72b839c9d684d822
MashBung <buzzwole49@gmail.com>
2026-01-11 23:52:18 +0900

    Update __init__.py

--- blood_cell_classifier/__init__.py
+++ blood_cell_classifier/__init__.py
@@ -1,7 +1,8 @@
 """혈액 세포 분류 모델"""
 
-from .classifier import BloodCellClassifier, quick_predict
-from .model import BloodCNN
+#from .classifier import BloodCellClassifier, quick_predict
+#from .model import BloodCNN
+from .blood_cell_model import BloodCellClassifier
 
 __version__ = "1.0.0"
 __all__ = ["BloodCellClassifier", "BloodCNN", "quick_predict"]
